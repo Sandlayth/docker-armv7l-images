cc508d4b14c49994e7d6f220891da5f98e630911
Sandlayth <modepadu95@riseup.net>
2017-09-01 21:42:02 +0000

    Update alpine version to 3.6

--- update_images/update_alpine.sh
+++ update_images/update_alpine.sh
@@ -7,7 +7,7 @@ MINOR_VERSION="0"
 
 pushd ../docker-alpine
 
-curl https://nl.alpinelinux.org/alpine/v3.5/releases/armhf/alpine-minirootfs-3.5.0-armhf.tar.gz  > rootfs.tar.gz
+curl https://nl.alpinelinux.org/alpine/v3.6/releases/armhf/alpine-minirootfs-3.6.0-armhf.tar.gz  > rootfs.tar.gz
 
 docker build -t sandlayth/armv7l-alpine . && docker push sandlayth/armv7l-alpine && echo "Alpine successfully updated and pushed."
 
